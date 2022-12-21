32b23a0adde74399d7a1789e5acfc0eaf14b19f4
hxnCodes <hxncodes@gmail.com>
2022-12-21 20:57:22 +0500

    dice changed to object and nanoid added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import Die from "./Die";
 import "./App.css";
 import { useState } from "react";
+import { nanoid } from "nanoid";
 
 function App() {
   // state to save dice numbers
   const [dice, setDice] = useState(allNewDice);
 
-  // generate random numbers between 1 and 6
+  // generate random numbers between 1 and 6 and storing it as object
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push(Math.ceil(Math.random() * 6));
+      newDice.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid(),
+      });
     }
     return newDice;
   }
@@ -20,7 +25,9 @@ function App() {
   }
 
   // generating Die component with map function
-  const diceElements = dice.map((die) => <Die value={die} />);
+  const diceElements = dice.map((die) => (
+    <Die key={die.id} value={die.value} isHeld={die.isHeld} />
+  ));
 
   return (
     <main>
--- src/Die.jsx
+++ src/Die.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 // using destructured props in Die component
-export default function Die({ value }) {
+export default function Die({ value, isHeld }) {
+  const styles = { backgroundColor: isHeld ? "#59E391" : "white" };
   return (
-    <div className="die-face">
+    <div className="die-face" style={styles}>
       <h2 className="die-num">{value}</h2>
     </div>
   );
