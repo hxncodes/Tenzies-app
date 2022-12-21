c0b69edd1bd6f620503fcdf3fe6403c8f66e2675
hxnCodes <hxncodes@gmail.com>
2022-12-21 21:24:57 +0500

    dice hold functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -7,30 +7,55 @@ function App() {
   // state to save dice numbers
   const [dice, setDice] = useState(allNewDice);
 
+  // generating new die as object
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
+
   // generate random numbers between 1 and 6 and storing it as object
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push({
-        value: Math.ceil(Math.random() * 6),
-        isHeld: false,
-        id: nanoid(),
-      });
+      newDice.push(generateNewDie());
     }
     return newDice;
   }
 
+  // conditionally rolling dice
   function rollDice() {
-    setDice(allNewDice());
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.isHeld ? die : generateNewDie();
+      })
+    );
   }
 
-  // generating Die component with map function
+  function holdDice(id) {
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
+  }
+
+  // passing id of die component to holdDice function
   const diceElements = dice.map((die) => (
-    <Die key={die.id} value={die.value} isHeld={die.isHeld} />
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      holdDice={() => holdDice(die.id)}
+    />
   ));
 
   return (
     <main>
+    <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">{diceElements}</div>
       <button className="roll-dice" onClick={rollDice}>
         Roll
--- src/Die.jsx
+++ src/Die.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 // using destructured props in Die component
-export default function Die({ value, isHeld }) {
+export default function Die({ value, isHeld, holdDice }) {
   const styles = { backgroundColor: isHeld ? "#59E391" : "white" };
   return (
-    <div className="die-face" style={styles}>
+    <div className="die-face" style={styles} onClick={holdDice}>
       <h2 className="die-num">{value}</h2>
     </div>
   );
