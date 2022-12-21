c0ea93389611882d23c784d6ee4ef73295dd5e11
hxnCodes <hxncodes@gmail.com>
2022-12-21 20:42:57 +0500

    Dices are rendered through map

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
 import Die from "./Die";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  // state to save dice numbers
+  const [dice, setDice] = useState(allNewDice);
+
+  // generate random numbers between 1 and 6
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6));
+    }
+    return newDice;
+  }
+
+  function rollDice() {
+    setDice(allNewDice());
+  }
+
+  // generating Die component with map function
+  const diceElements = dice.map((die) => <Die value={die} />);
+
   return (
     <main>
-      <div className="dice-container">
-        <Die value="1" />
-        <Die value="2" />
-        <Die value="3" />
-        <Die value="4" />
-        <Die value="5" />
-        <Die value="6" />
-        <Die value="7" />
-        <Die value="8" />
-        <Die value="9" />
-        <Die value="10" />
-      </div>
+      <div className="dice-container">{diceElements}</div>
+      <button className="roll-dice" onClick={rollDice}>
+        Roll
+      </button>
     </main>
   );
 }
